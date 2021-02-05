68cb4417feeb00be6f222a0cf30db090702f0ffd
ShinjinE <shinjinethan@hotmail.com>
2021-02-05 09:33:17 -0700

    Soft shutdown with button press prototype (running program on boot requires changing a file in a location outside this directory). Pololu Mini Maestro drivers and software downloaded. Prototype for controling servos through maestro using a python script.

---
+++ test_code/maestro_test.py
@@ -0,0 +1,29 @@
+import maestro
+from time import sleep
+
+servo = maestro.Controller()
+# servo.setAccel(0,4)      #set servo 0 acceleration to 4
+# servo.setTarget(5,1792*4)  #set servo to move to center position
+# sleep(1)
+# servo.setTarget(5,2496*4)  #set servo to move to center position
+# sleep(1)
+# servo.setTarget(5,(1792+2496)*2)  #set servo to move to center position
+# servo.setSpeed(1,10)     #set speed of servo 1
+# x = servo.getPosition(1) #get the current position of servo 1
+# print(x)
+
+rangeMin = 1792*4
+rangeMax = 2496*4
+currPos = rangeMin
+for x in range(5):
+    while currPos < rangeMax:
+        servo.setTarget(5,currPos)
+        currPos += 1
+        sleep(.001)
+    # sleep(0.5)
+    while currPos > rangeMin:
+        servo.setTarget(5,currPos)
+        currPos -= 1
+        # sleep(.001)
+
+servo.close()
